d3e86e7b2ebf0adc9013a77cd43386743517af93
Cesar Gomez <cgope1985@gmail.com>
2023-06-21 19:04:08 -0500

    cleaning non used models

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,15 +1,11 @@
 import { useState } from 'react';
 import GuessInput from '@components/GuessInput/GuessInput';
 import GuessResults from '@components/GuessResults/GuessResults';
-import { GuessType } from '@models/models';
+
 const Game = () => {
   const [guesses, setGuesses] = useState<string[]>([]);
 
   const handleSubmitGuess = (tentativeGuess: string) => {
-    // const nextGuess = {
-    //   value: tentativeGuess,
-    //   id: `${tentativeGuess}-${Math.random()}`,
-    // };
     setGuesses([...guesses, tentativeGuess]);
   };
   return (
--- src/models/models.ts
+++
@@ -1,3 +0,0 @@
-export interface GuessType {
-  value: string;
-}
